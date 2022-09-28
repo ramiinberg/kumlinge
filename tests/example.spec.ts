8131d54f49b7bce0f967c0a47220752d8a9b2ca5
Rami Inberg <rami.inberg@gmail.com>
2022-09-28 11:13:55 +0300

    click event testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from '@playwright/test'
 
-test('simple basic test', async ( { page }) => {
+test('simple basic test', async ({ page }) => {
   // here goes the test code
   await page.goto('https://www.example.com')
   const pageTitle = await page.locator('h1')
   await expect(pageTitle).toContainText('Example Domain')
 })
 
+test('clicking on elemenets', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.click('#signin_button')
+  await page.click('text=Sign in')
+
+  const errorMessage = await page.locator('.alert-error')
+  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
