82817dea344f99903a87351b993763a262228d76
Rami Inberg <rami.inberg@gmail.com>
2022-09-28 12:43:35 +0300

    Assertions example testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -46,3 +46,17 @@ test('Working with Inputs', async ({ page }) => {
   const errorMessage = await page.locator('.alert-error')
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+
+test('Assertions', async ({ page }) => {
+  await page.goto('https://www.example.com')
+  await expect(page).toHaveURL('https://www.example.com')
+  await expect(page).toHaveTitle('Example Domain')
+
+  const element = await page.locator('h1')
+  await expect(element).toBeVisible()
+  await expect(element).toHaveText('Example Domain')
+  await expect(element).toHaveCount(1)
+
+  const nonExistingElement = await page.locator('h5')
+  await expect(nonExistingElement).not.toBeVisible()
+})
