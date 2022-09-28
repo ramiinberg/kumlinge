b79f2c7d139d401c5336a57bb46f4ee6ca0ecbb3
Rami Inberg <rami.inberg@gmail.com>
2022-09-28 12:52:50 +0300

    tags and annotations

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from '@playwright/test'
 
+// test.only
+// test.skip
+
+//npx playwring test --grep @myTag
+//npx playwring test --grep-inver @MyTag
+
 test('simple basic test', async ({ page }) => {
   // here goes the test code
   await page.goto('https://www.example.com')
@@ -16,47 +22,49 @@ test('clicking on elemenets', async ({ page }) => {
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
 
-// test('Selectors',async ({ page }) => {
-//   // text
-//   await page.click("text=some text")
+test.skip('Selectors', async ({ page }) => {
+  // text
+  await page.click('text=some text')
 
-//   // CSS selectors
-//   await page.click('button')
-//   await page.click('#id')
-//   await page.click('.class')
+  // CSS selectors
+  await page.click('button')
+  await page.click('#id')
+  await page.click('.class')
 
-//   //Only visible CSS selector
-//   await page.click('.submit-button:visible')
+  //Only visible CSS selector
+  await page.click('.submit-button:visible')
 
-//   // Combinations
-//   await page.click('#username .first')
+  // Combinations
+  await page.click('#username .first')
 
-//   // XPath
-//   await page.click('//button')
-// })
-
-test('Working with Inputs', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/index.html')
-  await page.click('#signin_button')
-
-  await page.type('#user_login', 'some username')
-  await page.type('#user_password', 'some password')
-  await page.click('text=Sign in')
-
-  const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  // XPath
+  await page.click('//button')
 })
 
-test('Assertions', async ({ page }) => {
-  await page.goto('https://www.example.com')
-  await expect(page).toHaveURL('https://www.example.com')
-  await expect(page).toHaveTitle('Example Domain')
+test.describe('My first test suite', () => {
+  test('Working with Inputs', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
 
-  const element = await page.locator('h1')
-  await expect(element).toBeVisible()
-  await expect(element).toHaveText('Example Domain')
-  await expect(element).toHaveCount(1)
+    await page.type('#user_login', 'some username')
+    await page.type('#user_password', 'some password')
+    await page.click('text=Sign in')
 
-  const nonExistingElement = await page.locator('h5')
-  await expect(nonExistingElement).not.toBeVisible()
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  })
+
+  test('Assertions @myTag', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    await expect(page).toHaveURL('https://www.example.com')
+    await expect(page).toHaveTitle('Example Domain')
+
+    const element = await page.locator('h1')
+    await expect(element).toBeVisible()
+    await expect(element).toHaveText('Example Domain')
+    await expect(element).toHaveCount(1)
+
+    const nonExistingElement = await page.locator('h5')
+    await expect(nonExistingElement).not.toBeVisible()
+  })
 })
