f04be6cd12fedd2a5589ca4b2bd3d635585c59bc
ramiinberg <rami.inberg@gmail.com>
2022-09-29 15:08:13 +0300

    screenshot for single element

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -5,4 +5,10 @@ test.describe('Visual regression testing example', () => {
     await page.goto('https://www.example.com')
     expect(await page.screenshot()).toMatchSnapshot('homepage.png')
   })
+
+  test('single element snapshot', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    const pageElement = await page.$('h1')
+    expect(await pageElement?.screenshot()).toMatchSnapshot('page-title.png')
+  })
 })
