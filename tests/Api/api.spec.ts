21e73c920d0a6ddf35186a80a649465e93abcb14
ramiinberg <rami.inberg@gmail.com>
2022-09-30 14:06:01 +0300

    post api tests

--- tests/Api/api.spec.ts
+++ tests/Api/api.spec.ts
@@ -22,4 +22,39 @@ test.describe.parallel('API Testing', () => {
     expect(responseBody.data.last_name).toBe('Bluth')
     expect(responseBody.data.email).toBeTruthy()
   })
+
+  test('POST Request - Create new user', async ({ request }) => {
+    const response = await request.post(`${baseURL}/users`, {
+      data: {
+        id: 1000
+      }
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(201)
+    expect(responseBody.id).toBe(1000)
+    expect(responseBody.createdAt).toBeTruthy()
+  })
+
+  test('POST Request - Login', async ({ request }) => {
+    const response = await request.post(`${baseURL}/login`, {
+      data: {
+        email: 'eve.holt@reqres.in',
+        password: 'cityslicka'
+      }
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(200)
+    expect(responseBody.token).toBeTruthy()
+  })
+
+  test('Post request - Login fail', async ({ request }) => {
+    const response = await request.post(`${baseURL}/login`, {
+      data: {
+        email: 'eve.holt@reqres.in'
+      }
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(400)
+    expect(responseBody.error).toBe('Missing password')
+  })
 })
