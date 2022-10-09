e3e5ab5e57bc580a1360fec34af4eb2345a32f36
ramiinberg <rami.inberg@gmail.com>
2022-10-09 11:27:43 +0300

    table styling

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,13 +1,26 @@
 import './index.scss'
+import data from '../../data/data'
 
 function Table() {
   return (
-    <div className='table'>
-      <div className='table__header'>
-        <div className='table__cell'>Tärkeät</div>
-        <div className='table__cell'>Kenen homma</div>
-      </div>
-    </div>
+    <table className='table'>
+      <thead>
+        <tr className='table__header'>
+          <th className='table__header__cell1'>Kuvaus</th>
+          <th className='table__header__cell2'>Tärkeysaste</th>
+          <th className='table__header__cell3'>Kuka</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => (
+          <tr key={item.id} className='table__data'>
+            <td className='table__data__cell1'>{item.text}</td>
+            <td className='table__data__cell2'>{item.importance}</td>
+            <td className='table__data__cell3'>{item.who}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
