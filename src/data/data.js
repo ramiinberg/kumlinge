a345e5ebeedd3b9e390af8c276afc3a72935f516
ramiinberg <rami.inberg@gmail.com>
2022-10-09 10:00:30 +0300

    added first data

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    id: 1,
+    text: 'Sytyttimiä',
+    importance: 'high',
+    who: '???'
+  },
+  {
+    id: 2,
+    text: 'Talouspaperi',
+    importance: 'high',
+    who: '???'
+  },
+  {
+    id: 3,
+    text: 'Vessapaperia',
+    importance: 'high',
+    who: '???'
+  },
+  {
+    id: 4,
+    text: 'PAREMPIA sytyspaloja',
+    importance: 'medium',
+    who: '???'
+  },
+  {
+    id: 5,
+    text: 'Hyttysverkko keskimökin oveen',
+    importance: 'high',
+    who: '???'
+  },
+  {
+    id: 6,
+    text: 'Mausteet',
+    importance: 'medium',
+    who: '???'
+  },
+  {
+    id: 7,
+    text: 'Linnunpönttö',
+    importance: 'low',
+    who: '???'
+  },
+  {
+    id: 8,
+    text: 'Köysi',
+    importance: 'medium',
+    who: 'Allu'
+  },
+  {
+    id: 9,
+    text: 'Mustaa kumia traileriin',
+    importance: 'medium',
+    who: 'Allu'
+  },
+  {
+    id: 10,
+    text: 'Kaasupullo 2kpl',
+    importance: 'high',
+    who: 'Klasu'
+  },
+  {
+    id: 11,
+    text: 'Akku 115 ampperia',
+    importance: 'high',
+    who: '???'
+  },
+  {
+    id: 12,
+    text: 'EHKÄ akkukotelo',
+    importance: 'medium',
+    who: '???'
+  },
+  {
+    id: 13,
+    text: 'Hanskat',
+    importance: 'medium',
+    who: 'Allu'
+  },
+  {
+    id: 14,
+    text: 'Biltema kumi suojaus laituriin',
+    importance: 'high',
+    who: '???'
+  },
+  {
+    id: 15,
+    text: 'Leipiä menomatkalle',
+    importance: 'medium',
+    who: '???'
+  }
+]
